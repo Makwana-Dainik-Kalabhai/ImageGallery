3c4689abb4a37feb47fa7ce71e951e5ca3b328c6
Makwana Dainik  Kalabhai <dainikmakwana31@gmail.com>
2024-12-05 18:39:10 +0530

    msg

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,6 +5,7 @@ function Hero(props) {
     let [page, setPage] = useState(1);
     let [data, setData] = useState([]);
     let [search, setSearch] = useState("");
+    let [downOptions, setDownOptions] = useState("");
 
     async function fetchApi() {
         props.changeLoad(30);
@@ -69,15 +70,30 @@ function Hero(props) {
                     {data.map((e, i) => {
                         return (
                             <div className="card">
-                                <a href={e.links.download}>
-                                    <img src={e.urls.regular} alt={e.alt_description && e.alt_description} />
-                                    <a className="btn btn-light download" href={e.links.download} download><i className="fa-solid fa-arrow-down"></i></a>
 
-                                    <div className="likes">
-                                        <i className="fa-solid fa-heart"></i>
-                                        <span>{e.likes}</span>
-                                    </div>
-                                </a>
+                                <div className="profile">
+                                    {e.sponsorship?.sponsor?.profile_image?.large ? <img className="profile-img" src={e.sponsorship.sponsor.profile_image.large} /> : ""}
+                                    {e.sponsorship?.sponsor?.name ? <span>{e.sponsorship.sponsor.name}</span> : ""}
+                                </div>
+
+                                <img className="original-img" src={e.urls.regular} alt={e.alt_description && e.alt_description} onClick={()=> {
+                                    setDownOptions("");
+                                }} />
+
+                                <button className="btn btn-light download" onClick={()=> {
+                                    setDownOptions(e.links.download);
+                                }}><i className="fa-solid fa-arrow-down"></i></button>
+
+                                <div className="likes">
+                                    <i className="fa-solid fa-heart"></i>
+                                    <span>{e.likes}</span>
+                                </div>
+                                <div className="download-options" style={{display: downOptions===e.links.download?"block":"none"}}>
+                                    <a href={`${e.links.download}&amp;force=true&amp;w=640&amp;h=480`} download="">Small <span>(640 x 480)</span></a>
+                                    <a href={`${e.links.download}&amp;force=true&amp;w=1920&amp;h=1400`} download="">Medium <span>(1920 x 1440)</span></a>
+                                    <a href={`${e.links.download}&amp;force=true&amp;w=2400&amp;h=1800`} download="">Large <span>(2400 x 1800)</span></a>
+                                    <a href={`${e.links.download}&amp;force=true&amp;w${e.width}&amp;h=${e.height}`} download="">Original <span>({e.width} x {e.height})</span></a>
+                                </div>
                             </div>
                         )
                     })}
